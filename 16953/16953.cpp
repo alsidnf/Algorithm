758ace32fa11113f1275f137c42431d0b2eb4785
우준하 <alsidnf@gmail.com>
2021-06-13 15:37:08 +0900

    1695, 20040

---
+++ 16953/16953.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	unsigned int A, B;
+	cin >> A >> B;
+	int cnt = 1;
+	while (A < B) {
+		++cnt;
+		if (B % 2 == 0) B /= 2;
+		else if (B % 10 == 1) B /= 10;
+		else break;
+	}
+	printf("%d", A == B ? cnt : -1);
+}
---
+++ 20040/20040.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+int N, M;
+int P[500000];
+int find(int n) {
+	if (P[n] < 0) return n;
+	return P[n] = find(P[n]);
+}
+bool merge(int a, int b) {
+	a = find(a);
+	b = find(b);
+	if (a == b) return false;
+	if (P[a] < P[b]) {
+		P[a] += P[b];
+		P[b] = a;
+	}
+	else {
+		P[b] += P[a];
+		P[a] = b;
+	}
+	return true;
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int a, b;
+	int ans = 0;
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) P[i] = -1;
+	for (int i = 1; i <= M; i++) {
+		cin >> a >> b;
+		if (ans) continue;
+		if (!merge(a, b)) {
+			ans = i;
+		}
+	}
+	cout << ans;
+}
