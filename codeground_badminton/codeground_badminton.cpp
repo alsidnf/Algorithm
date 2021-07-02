002aef9b7ce6e7b89eb6c254d27697c7d0d3f675
우준하 <alsidnf@gmail.com>
2021-07-02 14:02:43 +0900

    codeground 연습문제 배드민턴

---
+++ codeground_badminton/codeground_badminton.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int T, test_case;
+	cin >> T;
+	string str;
+	for (test_case = 0; test_case < T; test_case++)
+	{
+		cin >> str;
+		cout << "Case #" << test_case + 1 << endl;
+		int a = 0, b = 0, ans = 0;
+		for (char ch : str) {
+			if (ch == 'A') {
+				if (++a == 21) {
+					ans = 1;
+					cout << "Alice\n";
+					break;
+				}
+			}
+			else {
+				if (++b == 21) {
+					ans = 2;
+					cout << "Bob\n";
+					break;
+				}
+			}
+		}
+		if (ans == 0) cout << "Playing\n";
+	}
+	return 0;
+}
