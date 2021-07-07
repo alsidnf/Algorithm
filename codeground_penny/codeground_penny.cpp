b109a2d11e242634a925137dce3faffe5814ea77
우준하 <alsidnf@gmail.com>
2021-07-07 16:14:17 +0900

    practice 문제

---
+++ codeground_penny/codeground_penny.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+const int coin[4] = { 10,50,100,500 };
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int T;
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++) {
+		int ans[4] = { 0 };
+		int a;
+		cin >> a;
+		for (int i = 3; i >= 0; --i) {
+			if (a / coin[i]) {
+				ans[i] = a / coin[i];
+				a %= coin[i];
+			}
+		}
+		cout << "Case #" << tc << "\n";
+		for (int i = 0; i < 4; i++) cout << ans[i] << " ";
+		cout << "\n";
+	}
+}
---
+++ practice_hamburger/practice_hamburger.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int TC, b, m, c;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> TC;
+	for (int tc = 1; tc <= TC; tc++) {
+		cin >> b >> m >> c;
+		int ans = min({b / 2, m, c / 3});
+		cout << "Case #" << tc << "\n" << ans << "\n";
+	}
+}
