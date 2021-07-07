58e312ecb0714b724effdeb866f4979363cebaf5
우준하 <alsidnf@gmail.com>
2021-07-07 22:09:06 +0900

    codeground practice 파티셰리

---
+++ codeground_partyshaery/codeground_partyshaery.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool dfs(int n) {
+	if (n == 0) return true;
+	if (n < 0) return false;
+	if (dfs(n - 11)) return true;
+	if (dfs(n - 9)) return true;
+	if (dfs(n - 7)) return true;
+	return false;
+}
+
+int main(int argc, char** argv)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int T, test_case;
+	cin >> T;
+	for (test_case = 1; test_case <= T; test_case++)
+	{
+		int N;
+		cin >> N;
+		cout << "Case #" << test_case << "\n";
+		cout << (dfs(N)?"YES":"NO") << "\n";
+	}
+
+	return 0;
+}
